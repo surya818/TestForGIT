e52f9bfb3847a5137d4e2bf39cf248339c1ea0b2
surya818 <suryakiran.rekha@gmail.com>
2015-09-21 13:56:54 +0530

    commit2

--- src/pack1/Arrays.java
+++ src/pack1/Arrays.java
@@ -1,20 +1,25 @@
 package pack1;
 
+import java.util.ArrayList;
+
 public class Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int[] bab = {1,3,4,2,1,6,9,0,1,0};
+int[] bab = {1,3,4,2,1,6,9,0,1,0,0,0,0,0,3,3};
 getDups(bab);
 	}
 
 	public static void getDups(int[]arr){
 		int[] vals = new int[256];
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(int tmp: arr){
-			if(vals[tmp]==1){
+			if(vals[tmp]==1 && (!list.contains(tmp))){
 				System.out.println(tmp);
+				list.add(tmp);
 			}
 			vals[tmp]=1;
+			
 		}
 	}
 }
